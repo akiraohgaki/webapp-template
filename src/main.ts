947ee5185d84df635b17b7ebd60ef93501a41567
Akira Ohgaki <akiraohgaki@gmail.com>
2024-09-26 04:58:04 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log('main');
+import Counter from './Counter.ts';
+
+const counter = new Counter();
+
+const button = document.createElement('button');
+button.textContent = 'Click me!';
+button.addEventListener('click', () => {
+  counter.increment();
+  button.textContent = `${counter.value} Clicked`;
+});
+
+document.body.appendChild(button);
