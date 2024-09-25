0d75c91ed06de865babcc27ceff4442a833815e3
Akira Ohgaki <akiraohgaki@gmail.com>
2024-09-26 04:57:52 +0900

    Add Counter.ts

---
+++ src/Counter.ts
@@ -0,0 +1,19 @@
+export default class Counter {
+  #value = 0;
+
+  constructor(initialValue: number = 0) {
+    this.#value = initialValue;
+  }
+
+  get value(): number {
+    return this.#value;
+  }
+
+  increment(): void {
+    this.#value = this.#value + 1;
+  }
+
+  decrement(): void {
+    this.#value = this.#value - 1;
+  }
+}
